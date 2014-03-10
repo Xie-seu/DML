35e9059d15b95aa8a9f76b8f5c381cd828429ffa
GellerX <hn.kailang@gmail.com>
2014-03-10 10:42:17 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DML::Application.config.secret_token = '1fd1dd50791ecf46b6ca92c4daec3462a8844b00f821633ba1f957541c9b30ce8c3acd3f11a7a566198533b8a1746379593299d43b31b4c2a2148d0aece3d9b1'
